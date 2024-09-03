44a9aba52aceb47edb0edd190396468d5d99d142
Katie Sipos <katie.sipos@gmail.com>
2024-09-03 19:25:04 +0000

    update macro

--- macros/rolling_average_n_periods.sql
+++ macros/rolling_average_n_periods.sql
@@ -1,7 +1,7 @@
-{% macro rolling_average_n_periods(column_name, partition_by, order_by='created_at', time_period=6) %}
+{% macro rolling_average_n_periods(column_name, partition_by, order_by, time_period=6, time_range='day') %}
     avg( {{ column_name }} ) OVER (
                 PARTITION BY {{ partition_by }}
                 ORDER BY {{ order_by }}
                 ROWS BETWEEN {{ time_period }} PRECEDING AND CURRENT ROW
-            ) AS avg_{{ time_period }}_periods_{{ column_name }}
+            ) AS avg_{{ time_period }}_{{ time_range }}_{{ column_name }}
 {% endmacro %}
