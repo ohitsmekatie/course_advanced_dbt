7a5a10f295b87b413b65d7070a9b36feec8d580a
Katie Sipos <katie.sipos@gmail.com>
2024-09-22 16:51:40 +0000

    refactor mrr model

--- models/marts/finance/fct_mrr_monthly.sql
+++ models/marts/finance/fct_mrr_monthly.sql
@@ -3,57 +3,8 @@
 
 -- This model is created following the dbt MRR playbook: https://www.getdbt.com/blog/modeling-subscription-revenue/
 
-WITH
-
--- Import CTEs
--- Get raw monthly subscriptions
-monthly_subscriptions AS (
-    SELECT
-        subscription_id,
-        user_id,
-        starts_at,
-        ends_at,
-        plan_name,
-        pricing,
-        {{ transform_date('month', 'starts_at') }} AS start_month,
-        {{ transform_date('month', 'ends_at') }} AS end_month
-    FROM
-        {{ ref('dim_subscriptions') }}
-    WHERE
-        billing_period = 'monthly'
-),
-
--- Use the dates spine to generate a list of months
-months AS (
-    SELECT
-        calendar_date AS date_month
-    FROM
-        {{ ref('int_dates') }}
-    WHERE
-        day_of_month = 1
-),
-
--- Logic CTEs
--- Create subscription period start_month and end_month ranges
-subscription_periods AS (
-    SELECT
-        subscription_id,
-        user_id,
-        plan_name,
-        pricing AS monthly_amount,
-        starts_at,
-        ends_at,
-        start_month,
-
-        -- For users that cancel in the first month, set their end_month to next month because the subscription remains active until the end of the first month
-        -- For users who haven't ended their subscription yet (end_month is NULL) set the end_month to one month from the current date (these rows will be removed from the final CTE)
-        CASE
-            WHEN start_month = end_month THEN DATEADD('month', 1, end_month)
-            WHEN end_month IS NULL THEN DATE(DATEADD('month', 1, DATE_TRUNC('month', CURRENT_DATE)))
-            ELSE end_month
-        END AS end_month
-    FROM
-        monthly_subscriptions
+with subscription_periods AS (
+    select * from {{ ref('subscription_periods') }}
 ),
 
 -- Determine when given subscription plan's first and most recent months
@@ -67,6 +18,15 @@ subscribers AS (
         subscription_periods
     GROUP BY
         1, 2
+),
+
+months AS (
+    SELECT
+        calendar_date AS date_month
+    FROM
+        {{ ref('int_dates') }}
+    WHERE
+        day_of_month = 1
 ),
 
 -- Create one record per month between a subscriber's first and last month
